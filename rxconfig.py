d7fc4a064d9a242a7c5f3af807e6f1e1b7b31cb5
pstreb <pablo.streb@gmail.com>
2026-01-16 19:27:44 -0300

    fix: configure Render deployment with correct port binding

--- rxconfig.py
+++ rxconfig.py
@@ -2,14 +2,25 @@
 import os
 from reflex.constants import LogLevel
 
+# Detecta a URL do Render automaticamente
+render_url = os.getenv("RENDER_EXTERNAL_URL")
+is_production = render_url is not None
+
 config = rx.Config(
     app_name="app",
     
-    # Simplificado para Render
+    # URLs para produção
+    api_url=render_url if is_production else "http://localhost:8000",
+    deploy_url=render_url if is_production else "http://localhost:3000",
+    
+    # Porta do Render
     backend_port=int(os.getenv("PORT", 8000)),
     backend_host="0.0.0.0",
     
-    # Remove CORS temporariamente
+    # CORS
+    cors_allowed_origins=["*"],
+    
+    # Simplificado
     disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
     telemetry_enabled=False,
     loglevel=LogLevel.INFO,
