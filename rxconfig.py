75324db240a6c5413188f6054af969048ac2f2ee
pstreb <pablo.streb@gmail.com>
2026-01-16 19:25:43 -0300

    fix: simplify config for Render

--- rxconfig.py
+++ rxconfig.py
@@ -2,32 +2,15 @@
 import os
 from reflex.constants import LogLevel
 
-is_production = os.getenv("RENDER") == "true" or os.getenv("RENDER_EXTERNAL_URL") is not None
-
-if is_production:
-    external_url = os.getenv("RENDER_EXTERNAL_URL", "https://ranking-system-y7h1.onrender.com")
-    api_url = external_url
-    deploy_url = external_url
-else:
-    api_url = "http://localhost:8000"
-    deploy_url = "http://localhost:3000"
-
 config = rx.Config(
-    app_name="app",  # ← MUDOU DE "ranking_system" PARA "app"
+    app_name="app",
     
-    api_url=api_url,
-    deploy_url=deploy_url,
+    # Simplificado para Render
     backend_port=int(os.getenv("PORT", 8000)),
-    frontend_port=3000,
     backend_host="0.0.0.0",
     
-    cors_allowed_origins=[
-        "http://localhost:3000",
-        "https://ranking-system-y7h1.onrender.com",
-    ],
-    
+    # Remove CORS temporariamente
     disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
-    db_url=os.getenv("DATABASE_URL", "sqlite:///reflex.db"),
     telemetry_enabled=False,
-    loglevel=LogLevel.INFO if is_production else LogLevel.DEBUG,
+    loglevel=LogLevel.INFO,
 )
